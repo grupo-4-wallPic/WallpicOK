471ec671c012a63c8390179c9e9e22975885afa0
renzoIrusta <renzoirusta22@gmail.com>
2020-09-05 21:10:56 -0300

    funciones agregadas product

--- Wallpic/public/JS/producto.js
+++ Wallpic/public/JS/producto.js
@@ -1,16 +1,20 @@
 let precio = document.querySelector('.main-price')
 let select = document.querySelector('.select-size')
 let checkPrice = document.querySelector('.checkPrice')
+let checkSize = document.querySelector('.checkSize')
 
-      console.log(select.value)
+    //   console.log(select.value)
 // size.forEach(size => {
     select.addEventListener('change', function (){
         let valores = select.value.split(',');
-        console.log(valores[1])
+        // console.log(valores[1])
         precio.textContent = '$' 
         precio.textContent += valores[0]
         checkPrice.value = valores[0]
-        select.value = 'hola'
+        checkSize.value = valores[1]
+
+        select.setAttribute("value", valores[1])
+        
     })
 
 // })
